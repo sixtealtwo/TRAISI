2a2d0e46e96673493b74c51affd596baf7fcec11
Siva Srikukenthiran <siva.srikukenthiran@mail.utoronto.ca>
2018-04-08 19:18:36 -0400

    Removal of files for notification popup; Addition of user-info component for transfer from QuickApp

---
+++ src/TRAISI/ClientApp/src/app/user-info/user-info.component.spec.ts
@@ -0,0 +1,26 @@
+/// <reference path="../../../node_modules/@types/jasmine/index.d.ts" />
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserInfoComponent } from './user-info.component';
+
+describe('UserInfoComponent', () => {
+  let component: UserInfoComponent;
+  let fixture: ComponentFixture<UserInfoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UserInfoComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
