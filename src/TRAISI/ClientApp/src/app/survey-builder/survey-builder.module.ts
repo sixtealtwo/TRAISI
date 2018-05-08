1f8c3ec896babce66bfa021b7654f74739f9939d
Brendan Reilly <brendan.reilly@utoronto.ca>
2018-05-08 16:03:56 -0400

    Survey manager builder link now loads the survey-builder component

--- src/TRAISI/ClientApp/src/app/survey-builder/survey-builder.module.ts
+++ src/TRAISI/ClientApp/src/app/survey-builder/survey-builder.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SurveyBuilderComponent } from './survey-builder.component';
+import {ROUTES} from "../survey-builder/survey-builder.routes";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    ROUTES,
   ],
-  declarations: []
+  declarations: [SurveyBuilderComponent]
 })
 export class SurveyBuilderModule { }
