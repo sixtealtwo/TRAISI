e2b436b21ffc34fd445ea40b347f6484a1c33c20
Brendan Reilly <brendan.benting@gmail.com>
2018-05-22 03:15:01 -0400

    Added endpoints to survey-viewer-endpoint-service

--- src/TRAISI/ClientApp/src/app/survey-viewer/services/survey-viewer-endpoint.service.ts
+++ src/TRAISI/ClientApp/src/app/survey-viewer/services/survey-viewer-endpoint.service.ts
@@ -2,11 +2,46 @@ import { EndpointFactory } from 'app/services/endpoint-factory.service';
 import { Injectable, Injector } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ConfigurationService } from 'app/services/configuration.service';
+import {catchError} from "rxjs/internal/operators/catchError";
+import {Observable} from "rxjs/index";
 
 @Injectable()
 export class SurveyViewerEndpointService extends EndpointFactory
 {
+	private readonly _surveyViewQuestionsUrl: string = '/api/survey-viewer/questions';
+
+	get surveyViewQuestionsUrl() { return this.configurations.baseUrl + "/"+ this._surveyViewQuestionsUrl }
+
 	/**
+	 * Returns the endpoint for retrieving survey questions of a particular survey view
+	 * @param {number} surveyId
+	 * @returns {Observable<T>}
+	 */
+	getSurveyViewQuestionsEndpoint<T>(surveyId:number): Observable<T> {
+		let endpointUrl = `${this.surveyViewQuestionsUrl}/${surveyId}`;
+
+		return this.http.get<T>(endpointUrl, this.getRequestHeaders())
+			.pipe(catchError(error => {
+				return this.handleError(error, () => this.getSurveyViewQuestionsEndpoint(surveyId));
+			}));
+	}
+
+	/**
+	 * Returns a particular question specified by the passed index of a particular survey view.
+	 * @param {number} surveyId
+	 * @param {number} questionIndex
+	 * @returns {Observable<T>}
+	 */
+	getSurveyViewQuestionEndpoint<T>(surveyId:number,questionIndex:number): Observable<T> {
+		let endpointUrl = `${this.surveyViewQuestionsUrl}/${surveyId}/${questionIndex}`;
+
+		return this.http.get<T>(endpointUrl, this.getRequestHeaders())
+			.pipe(catchError(error => {
+				return this.handleError(error, () => this.getSurveyViewQuestionEndpoint(surveyId));
+			}));
+	}
+
+				/**
 	 * Service constructor
 	 * @param http
 	 * @param configurations
