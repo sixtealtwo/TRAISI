ed06262a8fb606b1e1e97547064d721887b32000
Brendan Reilly <brendan.reilly@utoronto.ca>
2018-05-22 14:29:38 -0400

    Added property to SurveyQuestionAttribute to specify if question uses custom builder view

--- src/TRAISI/SDK/Attributes/SurveyQuestionAttribute.cs
+++ src/TRAISI/SDK/Attributes/SurveyQuestionAttribute.cs
@@ -5,7 +5,11 @@ namespace TRAISI.SDK.Attributes
     [AttributeUsage(AttributeTargets.Class)]
     public class SurveyQuestionAttribute : Attribute
     {
-       
+        /// <summary>
+        /// Specifies whether ot not this SurveyQuestion has a custom builder view.
+        /// </summary>
+        /// <returns></returns>
+       public bool CustomBuilderView{get;set;} = false;
 
     }
 }
