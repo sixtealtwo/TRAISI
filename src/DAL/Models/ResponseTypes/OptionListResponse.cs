7893580bcb96d92699d842f0547b3c3de12a9ee0
Brendan Reilly <brendan.reilly@utoronto.ca>
2018-05-23 15:39:37 -0400

    Added OptionListResponse type (may  not need)

---
+++ src/DAL/Models/ResponseTypes/OptionListResponse.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using DAL.Models.Surveys;
+using Newtonsoft.Json.Linq;
+
+namespace DAL.Models.ResponseTypes {
+public class OptionListResponse : SurveyResponse {
+
+        /// <summary>
+        /// List of all OptionValues
+        /// </summary>
+        /// <returns></returns>
+        ICollection<string> OptionValues{get;set;}
+    }
+}
