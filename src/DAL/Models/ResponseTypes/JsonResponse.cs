70e9622d5f6e4ecf0f989a2b0b2e4c294a5a0438
Brendan Reilly <brendan.reilly@utoronto.ca>
2018-05-22 21:01:18 -0400

    Column type definition for JsonResponse added as jsonb

--- src/DAL/Models/ResponseTypes/JsonResponse.cs
+++ src/DAL/Models/ResponseTypes/JsonResponse.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using DAL.Models.Questions;
 using DAL.Models.Surveys;
 using Newtonsoft.Json.Linq;
 
 namespace DAL.Models.ResponseTypes {
     public class JsonResponse : SurveyResponse {
+
+        [Column (TypeName = "jsonb")]
         JObject Value { get; set; }
     }
 
