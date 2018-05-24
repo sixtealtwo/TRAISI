915d4152c9de0ef8a7f461edf603eb3b4320a0af
Brendan Reilly <brendan.reilly@utoronto.ca>
2018-05-24 06:58:20 -0400

    Moved questions to TRAISI.Questions project. Multiple target configurations for TRAISI.SDK lib/exe Skeleton for CLI tool created to aid in bundling

---
+++ src/TRAISI.SDK/CLITool/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TRAISI.SDK.CLITool
+{
+    public class Program
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine("In TRAISI TOOL.");
+        }
+    }
+}
