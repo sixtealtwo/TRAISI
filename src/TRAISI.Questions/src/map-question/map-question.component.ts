753374aaeb6c8f5f4cb4b3cab51bb9e22af6f730
Siva <siva.srikukenthiran@mail.utoronto.ca>
2018-06-16 18:12:14 -0400

    TSLint added to TRAISI.Questions; adjustment of marker graphic position; replacement of Mapbox attributes to hide access key.

--- src/TRAISI.Questions/src/map-question/map-question.component.ts
+++ src/TRAISI.Questions/src/map-question/map-question.component.ts
@@ -1,72 +1,70 @@
-
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Result } from 'ngx-mapbox-gl/app/lib/control/geocoder-control.directive';
 import { MapComponent } from 'ngx-mapbox-gl';
 import { LngLatLike, MapMouseEvent } from 'mapbox-gl';
 import { MapEndpointService } from '../services/mapservice.service';
 
 @Component({
-  selector: 'traisi-map-question',
-  template: require('./map-question.component.html').toString(), 
-  styles: [require('./map-question.component.scss').toString()]
+	selector: 'traisi-map-question',
+	template: require('./map-question.component.html').toString(),
+	styles: [require('./map-question.component.scss').toString()]
 })
-export class MapQuestionComponent implements OnInit {
-  readonly QUESTION_TYPE_NAME: string = 'Map Question';
+export class MapQuestionComponent implements OnInit, AfterViewInit {
+	readonly QUESTION_TYPE_NAME: string = 'Map Question';
 
-  typeName: string;
+	typeName: string;
 	icon: string;
 
 	public locationSearch: string;
-	public markerPosition: LngLatLike = [-79.40, 43.67];
-	
+	public markerPosition: LngLatLike = [-79.4, 43.67];
+
 	@ViewChild('mapbox') mapGL: MapComponent;
 	@ViewChild('geocoder') mapGeocoder: any;
 
-  constructor(private mapEndpointService: MapEndpointService) {
-    this.typeName = this.QUESTION_TYPE_NAME;
-    this.icon = 'map';
+	constructor(private mapEndpointService: MapEndpointService) {
+		this.typeName = this.QUESTION_TYPE_NAME;
+		this.icon = 'map';
 		console.log('loaded');
 	}
-	
-  ngOnInit() {
+
+	ngOnInit() {
 		this.configureMapSettings();
 	}
-	
-	private configureMapSettings(): void {
-		this.mapGL.zoom=[9];
-		this.mapGL.minZoom=7;
-		this.mapGL.center=[-79.40, 43.67];
-		this.mapGL.maxBounds = [[-81.115327, 43.044575], [-78.055546, 44.634225]];
-		this.mapGL.doubleClickZoom = false;
-		this.locationSearch="Toronto";
+
+	ngAfterViewInit() {
+
 	}
 
-	public locationFound(event: {result: Result}): void {
+	private configureMapSettings(): void {
+		this.mapGL.zoom = [9];
+		this.mapGL.minZoom = 7;
+		this.mapGL.center = [-79.4, 43.67];
+		this.mapGL.maxBounds = [[-81.115327, 43.044575], [-78.055546, 44.634225]];
+		this.mapGL.doubleClickZoom = false;
+		this.mapGL.attributionControl = false;
+		this.locationSearch = 'Toronto';
+	}
+
+	public locationFound(event: { result: Result }): void {
 		this.locationSearch = event['result'].place_name;
 		this.markerPosition = event['result'].center;
 	}
 
-	onDragStart(event: any){
+	onDragStart(event: any) {}
 
+	onDragEnd(event: MapMouseEvent) {
+		this.mapEndpointService.reverseGeocode(event.lngLat.lat, event.lngLat.lng).subscribe(result => {
+			this.locationSearch = result;
+			this.mapGeocoder.control._inputEl.value = result;
+		});
 	}
 
-	onDragEnd(event: MapMouseEvent){
-		this.mapEndpointService.reverseGeocode(event.lngLat.lat, event.lngLat.lng).subscribe( result => 
-			{
-				this.locationSearch = result;
-				this.mapGeocoder.control._inputEl.value = result;
-			});
-	}
+	onDrag(event: MapMouseEvent) {}
 
-	onDrag(event: MapMouseEvent){
-
-	}
-
-	mapClick(event: MapMouseEvent){
+	mapClick(event: MapMouseEvent) {
 		if (event.lngLat) {
 			this.markerPosition = event.lngLat;
 			this.onDragEnd(event);
 		}
 	}
-
 }
