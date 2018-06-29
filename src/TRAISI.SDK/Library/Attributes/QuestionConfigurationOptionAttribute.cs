5ee38932ba09184052ad1eb7b885700df980ad84
Brendan Reilly <brendan.benting@gmail.com>
2018-06-29 03:15:11 -0400

    Update names of questionconfiguration/questionsetting attributes in TRAISI.SDK, and TextQuestion

--- src/TRAISI.SDK/Library/Attributes/QuestionConfigurationOptionAttribute.cs
+++ src/TRAISI.SDK/Library/Attributes/QuestionConfigurationOptionAttribute.cs
@@ -4,13 +4,13 @@ using TRAISI.SDK.Enums;
 namespace TRAISI.SDK.Attributes
 {
     [AttributeUsage(AttributeTargets.Field)]
-    public class QuestionParameterAttribute : Attribute
+    public class QuestionConfigurationOptionAttribute : Attribute
     {
         public string ParameterName{get;set;}
 
         public string ParameterDescription{get;set;}
         private QuestionParameterType _parameterType;
-        public QuestionParameterAttribute(QuestionParameterType parameterType)
+        public QuestionConfigurationOptionAttribute(QuestionParameterType parameterType)
         {
                 this._parameterType = parameterType;
         }
