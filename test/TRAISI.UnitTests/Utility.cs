168d55cc0148f5aa34af3b9b5769b6dee332e30b
Brendan Reilly <brendan.reilly@utoronto.ca>
2018-07-07 07:15:09 -0400

    Added more definitions for moq survey repository

--- test/TRAISI.UnitTests/Utility.cs
+++ test/TRAISI.UnitTests/Utility.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
 using DAL;
+using DAL.Models.Surveys;
+using DAL.Repositories.Interfaces;
 using Moq;
+
 
 namespace TRAISI.UnitTests
 {
@@ -12,12 +16,59 @@ namespace TRAISI.UnitTests
         /// <returns></returns>
         public static IUnitOfWork CreateUnitOfWork()
         {
-            IUnitOfWork unitOfWork;
-            var mock = new Mock<ApplicationDbContext>();
-            unitOfWork = new UnitOfWork(mock.Object);
-            
+            var mock = new Mock<IUnitOfWork>();
+            mock.SetupProperty( p => p.Surveys, CreateSurveyRepository());
 
-            return unitOfWork;
+            return mock.Object;
+        }
+
+        /// <summary>
+        /// Creates a Moq Survey Repository
+        /// </summary>
+        /// <returns></returns>
+        public static ISurveyRepository CreateSurveyRepository()
+        {
+            List<Survey> moqSurveys = new List<Survey>() {
+                new Survey() {
+                    Id = 1,
+                    Name = "Test Survey 1",
+                    Title = "Survey Title",
+                    IsActive = true,
+                    IsOpen = true
+                },
+                new Survey() {
+                    Id = 2,
+                    Name = "Test Survey 2",
+                    Title = "Survey Title",
+                    IsActive = true,
+                    IsOpen = true
+                },new Survey() {
+                    Id = 3,
+                    Name = "Test Survey 3",
+                    Title = "Survey Title",
+                    IsActive = true,
+                    IsOpen = true
+                },
+                new Survey() {
+                    Id = 4,
+                    Name = "Test Survey 4",
+                    Title = "Survey Title",
+                    IsActive = true,
+                    IsOpen = true
+                }
+
+            };
+            var mock = new Mock<ISurveyRepository>();
+            mock.Setup(m => m.GetAllAsync()).ReturnsAsync(moqSurveys);
+            mock.Setup( m => m.GetAsync(It.IsAny<int>())).
+            ReturnsAsync((int i) => moqSurveys.Find(s => s.Id == i));
+
+            mock.Setup( m=> m.CountAsync()).ReturnsAsync(moqSurveys.Count);
+            mock.Setup( m=> m.Remove(It.IsAny<Survey>())).Callback<Survey>((s) => moqSurveys.Remove(s));
+
+            mock.Setup( m=> m.Update(It.IsAny<Survey>())).Callback<Survey>((s) => moqSurveys.Find(p => p.Id == s.Id));
+
+            return mock.Object;
         }
     }
 }
