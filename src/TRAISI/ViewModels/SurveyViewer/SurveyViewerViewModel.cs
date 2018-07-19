b927d135af6f731c20094e7446a8c78e8377723c
Brendan Reilly <brendan.benting@gmail.com>
2018-07-19 19:27:05 -0400

    Add Updated Mapper definitions

--- src/TRAISI/ViewModels/SurveyViewer/SurveyViewerViewModel.cs
+++ src/TRAISI/ViewModels/SurveyViewer/SurveyViewerViewModel.cs
@@ -9,5 +9,13 @@ namespace TRAISI.ViewModels.SurveyViewer
         public string ViewName {get;set;}
 
         public List<QuestionPartViewModel> Questions;
+
+        public string TitleText {get;set;}
+
+        public string TermsAndConditionsText {get;set;}
+
+        public string WelcomeText{get;set;}
+
+        public string SurveyCompletionText {get;set;}
     }
 }
