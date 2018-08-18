14369e34d740c1ff54b62627b700a51a349606bc
Siva <siva.srikukenthiran@mail.utoronto.ca>
2018-08-18 00:45:28 -0400

    e-charts english version loaded instead.  Replaced TTS TAZ Zones with PD.  echarts-gl added; 3d globe added as sample.

--- src/TRAISI/ClientApp/src/app/dashboard/dashboard.component.ts
+++ src/TRAISI/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as echarts from 'echarts';
+import 'echarts-gl';
 
 import { of as observableOf, Observable, Subject } from 'rxjs';
 import { HttpClient } from '../../../node_modules/@angular/common/http';
@@ -11,14 +12,23 @@ import { HttpClient } from '../../../node_modules/@angular/common/http';
 })
 export class DashboardComponent implements OnInit {
 	options = {};
+	options2 = {};
+	echartsInitOptions = {
+		renderer: 'canvas'
+	};
 	echartsInstance: any;
 
-	private _mapURL = 'assets/maps/tts-zones.json';
+	private _mapURL = 'assets/maps/tts-zones-pd.json';
+	private flightURL = 'https://ecomfe.github.io/echarts-examples/public/data-gl/asset/data/flights.json';
 	public mapLoaded: boolean = false;
 
 	constructor(private http: HttpClient) {}
 	getMapData(): Observable<any> {
 		return this.http.get(this._mapURL);
+	}
+
+	getFlightData(): Observable<any> {
+		return this.http.get(this.flightURL);
 	}
 
 	ngOnInit() {
@@ -32,15 +42,15 @@ export class DashboardComponent implements OnInit {
 			}
 
 			let zoneInfo = [];
-			let nameMap = [];
 			geoJson.features.forEach(element => {
-				zoneInfo.push({name: 'Zone ' + element.properties.name, value: randomValue() });
-				nameMap[element.properties.name] = 'Zone ' + element.properties.name;
+				zoneInfo.push({ name: element.properties.name, value: randomValue() });
 			});
-		
+
 			// update options:
 			this.options = {
-				tooltip: {},
+				tooltip: {
+					formatter: `{b}`
+				},
 				visualMap: {
 					min: 0,
 					max: 9000,
@@ -54,24 +64,38 @@ export class DashboardComponent implements OnInit {
 					},
 					calculable: true
 				},
+				toolbox: {
+					show: true,
+					orient: 'vertical',
+					top: 'center',
+					right: '20px',
+					z: 20,
+					zlevel: 20,
+					feature: {
+						dataView: {
+							readOnly: true,
+							title: 'data view'
+						},
+						saveAsImage: {}
+					}
+				},
 				geo: {
 					map: 'TTS',
 					roam: true,
-					nameMap: nameMap,
 					label: {
 						emphasis: {
-								textStyle: {
-										color: '#fff'
-								},
-								show: false
+							textStyle: {
+								color: '#fff'
+							},
+							show: false
 						}
-				},
+					},
 					itemStyle: {
 						normal: {
 							borderColor: 'rgba(0, 0, 0, 0.2)'
 						},
 						emphasis: {
-							areaColor: null,
+							areaColor: '#ff0000',
 							shadowOffsetX: 0,
 							shadowOffsetY: 0,
 							shadowBlur: 20,
@@ -89,6 +113,141 @@ export class DashboardComponent implements OnInit {
 						data: zoneInfo
 					}
 				]
+			};
+		});
+
+		this.getFlightData().subscribe(data => {
+			let airports = data.airports.map(function(item) {
+				return {
+					coord: [item[3], item[4]]
+				};
+			});
+			function getAirportCoord(idx) {
+				return [data.airports[idx][3], data.airports[idx][4]];
+			}
+
+			// Route: [airlineIndex, sourceAirportIndex, destinationAirportIndex]
+			let routesGroupByAirline = {};
+			data.routes.forEach(function(route) {
+				let airline = data.airlines[route[0]];
+				let airlineName = airline[0];
+				if (!routesGroupByAirline[airlineName]) {
+					routesGroupByAirline[airlineName] = [];
+				}
+				routesGroupByAirline[airlineName].push(route);
+			});
+
+			let pointsData = [];
+			data.routes.forEach(function(airline) {
+				pointsData.push(getAirportCoord(airline[1]));
+				pointsData.push(getAirportCoord(airline[2]));
+			});
+
+			let series = data.airlines
+				.map(function(airline) {
+					let airlineName = airline[0];
+					let routes = routesGroupByAirline[airlineName];
+
+					if (!routes) {
+						return null;
+					}
+					return {
+						type: 'lines3D',
+						name: airlineName,
+
+						effect: {
+							show: true,
+							trailWidth: 2,
+							trailLength: 0.15,
+							trailOpacity: 1,
+							trailColor: 'rgb(30, 30, 60)'
+						},
+
+						lineStyle: {
+							width: 1,
+							color: 'rgb(50, 50, 150)',
+							// color: 'rgb(118, 233, 241)',
+							opacity: 0.1
+						},
+						blendMode: 'lighter',
+
+						data: routes.map(function(item) {
+							return [airports[item[1]].coord, airports[item[2]].coord];
+						})
+					};
+				})
+				.filter(function(series) {
+					return !!series;
+				});
+			series.push({
+				type: 'scatter3D',
+				coordinateSystem: 'globe',
+				blendMode: 'lighter',
+				symbolSize: 2,
+				itemStyle: {
+					color: 'rgb(50, 50, 150)',
+					opacity: 0.2
+				},
+				data: pointsData
+			});
+
+			this.options2 = {
+				legend: {
+					selectedMode: 'single',
+					left: 'left',
+					data: Object.keys(routesGroupByAirline),
+					orient: 'vertical',
+					textStyle: {
+						color: '#fff'
+					}
+				},
+				globe: {
+					environment: 'assets/maps/globe-assets/starfield.jpg',
+
+					heightTexture: 'assets/maps/globe-assets/bathymetry_bw_composite_4k.jpg',
+
+					displacementScale: 0.1,
+					displacementQuality: 'high',
+
+					baseColor: '#000',
+
+					shading: 'realistic',
+					realisticMaterial: {
+						roughness: 0.2,
+						metalness: 0
+					},
+
+					postEffect: {
+						enable: true,
+						depthOfField: {
+							enable: false,
+							focalDistance: 150
+						}
+					},
+					temporalSuperSampling: {
+						enable: true
+					},
+					light: {
+						ambient: {
+							intensity: 0
+						},
+						main: {
+							intensity: 0.1,
+							shadow: false
+						},
+						ambientCubemap: {
+							texture: 'assets/maps/globe-assets/lake.hdr',
+							exposure: 1,
+							diffuseIntensity: 0.5,
+							specularIntensity: 2
+						}
+					},
+					viewControl: {
+						autoRotate: false
+					},
+					silent: true
+				},
+				series: series
 			};
 		});
 	}
