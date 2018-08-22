0f0e149a9055291655f31464dcf86d2603d71fc6
Siva <siva.srikukenthiran@mail.utoronto.ca>
2018-08-22 01:54:56 -0400

    Migration update: removal of Description field for individual question options.

---
+++ src/TRAISI/Migrations/20180822055410_QuestionOptionUpdate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TRAISI.Migrations
+{
+    public partial class QuestionOptionUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "QuestionOptions");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "QuestionOptions",
+                nullable: true);
+        }
+    }
+}
