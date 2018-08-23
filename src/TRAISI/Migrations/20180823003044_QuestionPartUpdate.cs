746010307c3b2c494e0327d3823b9f3ecdb9da7f
Siva <siva.srikukenthiran@mail.utoronto.ca>
2018-08-22 21:26:48 -0400

    Migrations update for Question Part changes.  Style update/fix to prevent scroll on ios when dragging in builder.

---
+++ src/TRAISI/Migrations/20180823003044_QuestionPartUpdate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TRAISI.Migrations
+{
+    public partial class QuestionPartUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "QuestionParts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "QuestionParts");
+        }
+    }
+}
