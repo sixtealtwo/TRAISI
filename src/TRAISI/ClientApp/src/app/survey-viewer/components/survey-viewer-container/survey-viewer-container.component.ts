7b809061982d24d5d6b6f31fdafdb45304e8c9da
Brendan Reilly <brendan.benting@gmail.com>
2018-08-24 10:58:41 -0400

    Using boostrap style library temporarily

--- src/TRAISI/ClientApp/src/app/survey-viewer/components/survey-viewer-container/survey-viewer-container.component.ts
+++ src/TRAISI/ClientApp/src/app/survey-viewer/components/survey-viewer-container/survey-viewer-container.component.ts
@@ -1,11 +1,12 @@
-import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
-import { SurveyViewerService } from '../../services/survey-viewer.service';
-import { QuestionLoaderService } from '../../services/question-loader.service';
-import { ActivatedRoute } from '@angular/router';
-import { SurveyErrorComponent } from '../survey-error/survey-error.component';
-import { SurveyStartPageComponent } from '../survey-start-page/survey-start-page.component';
+import {Component, ComponentFactoryResolver, OnInit, ViewEncapsulation} from '@angular/core';
+import {SurveyViewerService} from '../../services/survey-viewer.service';
+import {QuestionLoaderService} from '../../services/question-loader.service';
+import {ActivatedRoute} from '@angular/router';
+import {SurveyErrorComponent} from '../survey-error/survey-error.component';
+import {SurveyStartPageComponent} from '../survey-start-page/survey-start-page.component';
 
 @Component({
+	encapsulation: ViewEncapsulation.None,
 	selector: 'app-survey-viewer-container',
 	templateUrl: './survey-viewer-container.component.html',
 	styleUrls: ['./survey-viewer-container.component.scss'],
