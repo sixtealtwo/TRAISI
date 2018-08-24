24097417a64afeccdbc2118bb1851e570e0cc8a6
Brendan Reilly <brendan.benting@gmail.com>
2018-08-24 16:04:19 -0400

    New branch to split frontend into two separate apps - traisi-admin-app and traisi-survey-viewer-app Large overhaul of src code structure Currently compiles - still needs a few steps to connect with the dotnet core server

---
+++ src/TRAISI/ClientApp/src/e2e/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+/// <reference path="../node_modules/@types/jasmine/index.d.ts" />
+
+import { AppPage } from './app.po';
+
+describe('TRAISI', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  /*it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });*/
+});
