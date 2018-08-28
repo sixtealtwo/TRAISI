771c9a56b656566d305670492c73ac6798ccb9ce
Brendan Reilly <brendan.benting@gmail.com>
2018-08-28 19:54:25 -0400

    Cleaned up references to admin (from survey)

--- src/TRAISI/ClientApp/src/survey-viewer-app/app/app.module.ts
+++ src/TRAISI/ClientApp/src/survey-viewer-app/app/app.module.ts
@@ -2,13 +2,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {CoreModule} from "./core/core-module.module";
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {FormsModule} from "@angular/forms";
-import {LocalStoreManager} from "../../admin-app/app/services/local-store-manager.service";
-import {EndpointFactory} from "../../admin-app/app/services/endpoint-factory.service";
-import {AppRoutingModule} from "../../admin-app/app/app-routing.module";
+
 
 @NgModule({
 	declarations: [
@@ -20,13 +17,10 @@ import {AppRoutingModule} from "../../admin-app/app/app-routing.module";
 		HttpClientModule,
 		BrowserAnimationsModule,
 		FormsModule,
-		AppRoutingModule,
-		CoreModule
+
 	],
 	providers: [
 
-		LocalStoreManager,
-		EndpointFactory,
 	],
 	bootstrap: [AppComponent]
 })
