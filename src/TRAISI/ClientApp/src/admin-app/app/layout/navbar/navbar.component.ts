093a4100bbadac388cf64a4b839eea77880e0e63
Siva <siva.srikukenthiran@mail.utoronto.ca>
2018-09-29 16:25:14 -0400

    Adjustments to placing of colour picker.  Style changes for builder.  Ability to drag question config and new page dialogs added.

--- src/TRAISI/ClientApp/src/admin-app/app/layout/navbar/navbar.component.ts
+++ src/TRAISI/ClientApp/src/admin-app/app/layout/navbar/navbar.component.ts
@@ -102,7 +102,6 @@ export class NavbarComponent implements OnInit, AfterViewInit {
 		if (location._platformStrategy instanceof HashLocationStrategy) {
 			$newActiveLink = this.$el.find('a[href="#' + location._baseHref + location.path().split('?')[0] + '"]');
 		} else {
-			console.log(location.path());
 			$newActiveLink = this.$el.find('a[href="' + location._baseHref + location.path().split('?')[0] + '"]');
 		}
 
