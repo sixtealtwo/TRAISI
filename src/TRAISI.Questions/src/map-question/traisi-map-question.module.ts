6775470395976d09ce0e2b9052594d6feb7eab16
Brendan Reilly <brendan.benting@gmail.com>
2018-09-28 21:11:08 -0400

    styling fixes

--- src/TRAISI.Questions/src/map-question/traisi-map-question.module.ts
+++ src/TRAISI.Questions/src/map-question/traisi-map-question.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MapQuestionComponent } from './map-question.component';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
-
 import { HttpClientModule } from '@angular/common/http';
-
 import { CommonModule } from '@angular/common';
 import { MapEndpointService } from '../services/mapservice.service';
 
@@ -29,8 +27,9 @@ import { MapEndpointService } from '../services/mapservice.service';
 		NgxMapboxGLModule.forRoot({
 			accessToken:
 				// tslint:disable-next-line:max-line-length
-				'pk.eyJ1IjoiYnJlbmRhbmJlbnRpbmciLCJhIjoiY2oyOTlwdjNjMDB5cTMzcXFsdmRyM3NnNCJ9.NXgWTnWfvGRnNgkWdd5wKg' // Can also be set per map (accessToken input of mgl-map)
-			// geocoderAccessToken: 'TOKEN' // Optionnal, specify if different from the map access token, can also be set per mgl-geocoder (accessToken input of mgl-geocoder)
+				'pk.eyJ1IjoiYnJlbmRhbmJlbnRpbmciLCJhIjoiY2oyOTlwdjNjMDB5cTMzcXFsdmRyM3NnNCJ9.NXgWTnWfvGRnNgkWdd5wKg'
+				// Can also be set per map (accessToken input of mgl-map)
+
 		}),
 		HttpClientModule
 	]
