b9c1c4eacb6c909716cbe37c528077ecd25baab3
Brendan Reilly <brendan.benting@gmail.com>
2018-11-02 16:24:58 -0400

    Updated conditional evalutaor for all conditional types

--- src/TRAISI/ClientApp/src/survey-viewer-app/app/services/survey-viewer-conditional-evaluator.service.ts
+++ src/TRAISI/ClientApp/src/survey-viewer-app/app/services/survey-viewer-conditional-evaluator.service.ts
@@ -26,10 +26,47 @@ export class SurveyViewerConditionalEvaluator {
 				return this.evaluateContains(sourceData, value);
 			case 'doesNotContain':
 				return !this.evaluateContains(sourceData, value);
+			case 'isEqualTo':
+				return this.evaluateEquals(sourceData, value);
+			case 'contains':
+				return this.evaluateContains(sourceData, value);
+			case 'isNotEqualTo':
+				return !this.evaluateEquals(sourceData, value);
+			case 'isAnyOf':
+				return this.evaluateIsAnyOf(sourceData, value);
+			case 'isAllOf':
+				return this.evaluateIsAllOf(sourceData, value);
+			case 'inRange':
+				return this.evaluateInRange(sourceData, value);
+			case 'outsideRange':
+				return !this.evaluateInRange(sourceData, value);
+			case 'inBounds':
+				return this.evaluateInBounds(sourceData, value);
+			case 'outOfBounds':
+				return this.evaluateInBounds(sourceData, value);
 			default:
 				return false;
 		}
 	}
+
+	/*		if (this.responseType === 'String') {
+			this.dropDownListItems = ['Contains', 'Does Not Contain'];
+		} else if (this.responseType === 'Boolean') {
+			this.dropDownListItems = ['Is Equal To'];
+		} else if (this.responseType === 'Integer') {
+			this.dropDownListItems = ['Is Equal To', 'Is Not Equal To', 'Greater Than', 'Less Than'];
+		} else if (this.responseType === 'Decimal') {
+			this.dropDownListItems = ['Is Equal To', 'Is Not Equal To', 'Greater Than', 'Less Than'];
+		} else if (this.responseType === 'Location') {
+			this.dropDownListItems = ['In Bounds', 'Out Of Bounds'];
+		} else if (this.responseType === 'Json') {
+			this.dropDownListItems = ['Contains', 'Does Not Contain'];
+		} else if (this.responseType === 'OptionSelect') {
+			this.dropDownListItems = ['Is Equal To', 'Is Not Equal To'];
+		} else if (this.responseType === 'OptionList') {
+			this.dropDownListItems = ['Is Any Of', 'Is All Of'];
+		} else if (this.responseType === 'DateTime') {
+			this.dropDownListItems = ['In Range', 'Outside Range']; */
 
 	/**
 	 *
@@ -40,5 +77,26 @@ export class SurveyViewerConditionalEvaluator {
 		const val: boolean = sourceData.value.indexOf(value) >= 0;
 
 		return val;
+	}
+
+	private evaluateEquals(sourceData: any, value: any): boolean {
+		const val: boolean = sourceData === value;
+		return val;
+	}
+
+	private evaluateInBounds(sourceData: any, value: any): boolean {
+		return true;
+	}
+
+	private evaluateIsAnyOf(sourceData: any, value: any): boolean {
+		return true;
+	}
+
+	private evaluateIsAllOf(sourceData: any, value: any): boolean {
+		return true;
+	}
+
+	private evaluateInRange(sourceData: any, value: any): boolean {
+		return true;
 	}
 }
