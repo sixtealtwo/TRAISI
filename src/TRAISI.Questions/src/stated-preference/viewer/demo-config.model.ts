1ed4637fd7967ff73523958d65eeb17c58d13d92
Brendan Reilly <brendan.benting@gmail.com>
2019-03-08 12:16:55 -0500

    Added default demo config for testing sp render

---
+++ src/TRAISI.Questions/src/stated-preference/viewer/demo-config.model.ts
@@ -0,0 +1,13 @@
+import { StatedPreferenceConfig } from './stated-preference-config.model';
+
+let demoConfig: StatedPreferenceConfig = {
+	headers: [],
+	choices: [
+		{
+			label: 'Inner Text',
+			viewCondition: ''
+		}
+	]
+};
+
+export default demoConfig;
