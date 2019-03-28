e3b7846a4119a641c99c107b10bf67bf92352893
Brendan Reilly <brendan.benting@gmail.com>
2019-03-28 16:40:04 -0400

    Mode information added to distance matrix request

--- src/TRAISI/Helpers/GeoService.cs
+++ src/TRAISI/Helpers/GeoService.cs
@@ -7,60 +7,78 @@ using Geocoding.Google;
 using Microsoft.Extensions.Options;
 using RestSharp;
 
-namespace TRAISI.Helpers {
-    public interface IGeoService {
-        Task<string> ReverseGeocodeAsync (double latitude, double longitude);
+namespace TRAISI.Helpers
+{
+	public interface IGeoService
+	{
+		Task<string> ReverseGeocodeAsync(double latitude, double longitude);
 
-        Task<string> DistanceMatrix (List<string> origins, List<string> destinations);
-    }
+		Task<string> DistanceMatrix(List<string> origins, List<string> destinations);
+	}
 
-    public class GeoService : IGeoService {
+	public class GeoService : IGeoService
+	{
 
-        private static readonly string GOOGLE_API_URL = "https://maps.googleapis.com/maps/api";
+		private static readonly string GOOGLE_API_URL = "https://maps.googleapis.com/maps/api";
 
-        private readonly IGeocoder _geocoder;
-        private GeoConfig _config;
-        private readonly RestClient _googleApiClient;
+		private static readonly string GOOGLE_MODE_DRIVING = "driving";
+		private static readonly string GOOGLE_MODE_TRANSIT = "transit";
+		private static readonly string GOOGLE_MODE_WALKING = "walking";
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="config"></param>
-        public GeoService (IOptions<GeoConfig> config) {
-            _config = config.Value;
-            this._geocoder = new GoogleGeocoder () { ApiKey = _config.APIKey };
-            this._googleApiClient = new RestClient (GOOGLE_API_URL);
-        }
+		private static readonly string GOOGLE_MODE_BICYCLING = "bicycling ";
 
-        public async Task<string> ReverseGeocodeAsync (double latitude, double longitude) {
-            var result = await this._geocoder.ReverseGeocodeAsync (latitude, longitude);
-            return result.First ().FormattedAddress;
-        }
+		private readonly IGeocoder _geocoder;
+		private GeoConfig _config;
+		private readonly RestClient _googleApiClient;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="origins"></param>
-        /// <param name="destinations"></param>
-        /// <returns></returns>
-        public Task<string> DistanceMatrix (List<string> origins, List<string> destinations) {
-            var request = new RestRequest ("distancematrix/json", Method.GET);
-            request.AddParameter ("origins", String.Join ("|", origins.ToArray ()));
-            request.AddParameter ("destinations", String.Join ("|", destinations.ToArray ()));
-            request.AddParameter ("key", _config.APIKey);
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="config"></param>
+		public GeoService(IOptions<GeoConfig> config)
+		{
+			_config = config.Value;
+			this._geocoder = new GoogleGeocoder() { ApiKey = _config.APIKey };
+			this._googleApiClient = new RestClient(GOOGLE_API_URL);
+		}
 
-            var apiTask = new TaskCompletionSource<string> ();
-            _googleApiClient.ExecuteAsync (request, response => {
-                Console.WriteLine (response.Content);
-                apiTask.SetResult (response.Content);
-            });
-            return apiTask.Task;
+		public async Task<string> ReverseGeocodeAsync(double latitude, double longitude)
+		{
+			var result = await this._geocoder.ReverseGeocodeAsync(latitude, longitude);
+			return result.First().FormattedAddress;
+		}
 
-        }
-    }
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="origins"></param>
+		/// <param name="destinations"></param>
+		/// <returns></returns>
+		public Task<string> DistanceMatrix(List<string> origins, List<string> destinations)
+		{
+			var request = new RestRequest("distancematrix/json", Method.GET);
+			request.AddParameter("origins", String.Join("|", origins.ToArray()));
+			request.AddParameter("destinations", String.Join("|", destinations.ToArray()));
+			request.AddParameter("key", _config.APIKey);
+			request.AddParameter("mode", GOOGLE_MODE_BICYCLING);
+			request.AddParameter("mode", GOOGLE_MODE_DRIVING);
+			request.AddParameter("mode", GOOGLE_MODE_WALKING);
+			request.AddParameter("mode", GOOGLE_MODE_TRANSIT);
 
-    public class GeoConfig {
-        public string Provider { get; set; }
-        public string APIKey { get; set; }
-    }
+			var apiTask = new TaskCompletionSource<string>();
+			_googleApiClient.ExecuteAsync(request, response =>
+			{
+				Console.WriteLine(response.Content);
+				apiTask.SetResult(response.Content);
+			});
+			return apiTask.Task;
+
+		}
+	}
+
+	public class GeoConfig
+	{
+		public string Provider { get; set; }
+		public string APIKey { get; set; }
+	}
 }
