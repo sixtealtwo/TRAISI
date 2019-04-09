e8c028dcd1d00b372c672495c210b38647f388c4
Brendan Reilly <brendan.benting@gmail.com>
2019-04-09 14:25:10 -0400

    Updated config for db to use local settings

--- src/TRAISI/DesignTimeDbContextFactory.cs
+++ src/TRAISI/DesignTimeDbContextFactory.cs
@@ -2,44 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
 using DAL;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
-using Microsoft.EntityFrameworkCore;
-using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
 
+namespace TRAISI {
+	public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext> {
+		public ApplicationDbContext CreateDbContext (string[] args) {
+			Mapper.Reset ();
+			IConfigurationRoot configuration;
+			var cb = new ConfigurationBuilder ()
+				.SetBasePath (Directory.GetCurrentDirectory ())
 
-namespace TRAISI
-{
-	public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
-	{
-		public ApplicationDbContext CreateDbContext(string[] args)
-		{
-			Mapper.Reset();
+				.AddJsonFile ("appsettings.json");
 
-			IConfigurationRoot configuration = new ConfigurationBuilder()
-				.SetBasePath(Directory.GetCurrentDirectory())
-				.AddJsonFile("appsettings.json")
-				.AddJsonFile("appsettings.Development.json", optional: true)
-				.Build();
-
-			var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
-
-            Boolean.TryParse(configuration.GetSection("DevelopmentSettings").
-            GetSection("UseSqliteDatabaseProvider").Value, out bool development);
-
-            if (development)
-			{
-				builder.UseSqlite("Data Source=dev.db;");
+			if (File.Exists ("appsettings.local.json")) {
+				cb.AddJsonFile ("appsettings.local.json");
+			} else {
+				cb.AddJsonFile ("appsettings.json", optional : true);
 			}
-			else
-			{
-				builder.UseNpgsql(configuration["ConnectionStrings:DefaultConnection"], b => b.MigrationsAssembly("TRAISI"));
-			}
-			builder.UseOpenIddict();
+			configuration = cb.Build ();
+			var builder = new DbContextOptionsBuilder<ApplicationDbContext> ();
 
-			return new ApplicationDbContext(builder.Options);
+			Boolean.TryParse (configuration.GetSection ("DevelopmentSettings").GetSection ("UseSqliteDatabaseProvider").Value, out bool development);
+
+			if (development) {
+				builder.UseSqlite ("Data Source=dev.db;");
+			} else {
+				builder.UseNpgsql (configuration["ConnectionStrings:DefaultConnection"], b => b.MigrationsAssembly ("TRAISI"));
+			}
+			builder.UseOpenIddict ();
+
+			return new ApplicationDbContext (builder.Options);
 		}
 	}
 }
