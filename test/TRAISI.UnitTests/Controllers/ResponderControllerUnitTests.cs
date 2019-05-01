9ef444e56d8b5146b163091035d4acea87ea6591
Brendan Reilly <brendan.benting@gmail.com>
2019-05-01 11:50:43 -0400

    Added controller test cases

---
+++ test/TRAISI.UnitTests/Controllers/ResponderControllerUnitTests.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TRAISI.UnitTests.Controllers
+{
+    public class ResponderControllerUnitTests
+    {
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[Fact]
+		public async Task SaveResponse_ForRespondent_ReturnsSuccessResult() {
+
+			Assert.Equal(0,0);
+
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[Fact]
+		public async Task SavedResponse_ForRespondent_ReturnsResponse()
+		{
+
+			Assert.Equal(0, 0);
+
+		}
+    }
+}
