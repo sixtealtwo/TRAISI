e8572cb726ca6898a4720ab4a21d919db3bd9455
Oliver Liang <oliver@engturtle.com>
2019-05-22 14:31:14 -0400

    added build script for frequent project updates

--- project_build_update.sh
+++ project_build_update.sh
@@ -3,9 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd ${DIR}
+echo "Clean and restore project root"
 dotnet clean
 dotnet restore
 
+echo "Update TRAISI.SDK"
 cd ${DIR}/src/TRAISI.SDK
 dotnet build -c cli
 
@@ -13,12 +15,15 @@ cd ${DIR}/src/TRAISI.SDK/Module
 npm install
 npm run build
 
+echo "Update TRAISI.Questions"
 cd ${DIR}/src/TRAISI.Questions
 npm install
 npm run staging
 
+echo "Update TRAISI/ClientApp"
 cd ${DIR}/src/TRAISI/ClientApp
 npm install
 
+echo "Build Project"
 cd ${DIR}/src/TRAISI
 dotnet build
