0b6b1f689f375201aab036448a5b3a8fb6b4d802
Brendan Reilly <brendan.benting@gmail.com>
2019-05-24 15:36:32 -0400

    Auth interceptor uses the session service

--- src/TRAISI/ClientApp/src/survey-viewer-app/app/http-interceptors/auth-interceptor.ts
+++ src/TRAISI/ClientApp/src/survey-viewer-app/app/http-interceptors/auth-interceptor.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
+	HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
 } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
+import { SurveyViewerSession } from 'app/services/survey-viewer-session.service';
 
 /** Pass untouched request through to the next request handler. */
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  intercept(req: HttpRequest<any>, next: HttpHandler):
-    Observable<HttpEvent<any>> {
-    return next.handle(req);
-  }
+	/**
+	 *Creates an instance of AuthInterceptor.
+	 * @param {SurveyViewerSession} _session
+	 * @memberof AuthInterceptor
+	 */
+	public constructor(private _session: SurveyViewerSession) {
+
+	}
+
+	/**
+	 *
+	 *
+	 * @param {HttpRequest<any>} req
+	 * @param {HttpHandler} next
+	 * @returns {Observable<HttpEvent<any>>}
+	 * @memberof AuthInterceptor
+	 */
+	public intercept(req: HttpRequest<any>, next: HttpHandler):
+		Observable<HttpEvent<any>> {
+		return next.handle(req);
+	}
 }
