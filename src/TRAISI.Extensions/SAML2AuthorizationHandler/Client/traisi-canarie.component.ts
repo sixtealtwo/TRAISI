67c0eb0de6a8cb3202852f06df540295c8923025
Brendan Reilly <brendan.benting@gmail.com>
2019-08-28 11:15:30 -0400

    Removed extensions project, SAML2 moved to main TRAISI project

--- src/TRAISI.Extensions/SAML2AuthorizationHandler/Client/traisi-canarie.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'traisi-saml2-authorization',
-    template: require('traisi-saml2.component.html')
-})
-export class TraisiSAML2Component {
-
-}
--- src/TRAISI.Extensions/TripLinx/TripLinxGeocoder.cs
+++
@@ -1,9 +0,0 @@
-namespace TRAISI.Geo.Extensions
-{
-    /// <summary>
-    /// TripLinx based geocoder service loaded as a plugin.
-    /// </summary>
-    public class TripLinxGeocoder {
-
-    }
-}
