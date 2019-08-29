e394ff5797ef51af8c3e2b2fce92475558d975fc
Brendan Reilly <brendan.benting@gmail.com>
2019-08-29 01:48:00 -0400

    Update

--- src/TRAISI/Authorization/SAML2AuthorizationHandler.cs
+++ src/TRAISI/Authorization/SAML2AuthorizationHandler.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using TRAISI.SDK.Attributes;
 using TRAISI.SDK.Interfaces;
 
 namespace TRAISI.Authorization.Extensions
 {
-    [SurveyAuthorizationHandler(Name="SAML2")]
-    public class SAML2AuthorizationHandler : TRAISI.SDK.Interfaces.TraisiIAuthorizationHandler
+    [Route("api/[controller]")]
+    [SurveyAuthorizationHandler(Name = "SAML2")]
+    public class SAML2AuthorizationHandler : Controller, TRAISI.SDK.Interfaces.TraisiIAuthorizationHandler
     {
 
-        [ExtensionConfigurationAttribute(Name="Sign On URL", Description="URL of sign on location")]
-        public string SignOnURL {get;set;}
-        
+        [ExtensionConfigurationAttribute(Name = "Sign On URL", Description = "URL of sign on location")]
+        public string SignOnURL { get; set; }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [NonAction]
         public bool ShouldAuthorize(HttpContext s)
         {
             throw new NotImplementedException();
--- src/TRAISI/ClientApp/src/survey-viewer-app/app/modules/authorization/saml2/saml2-authorization.component.ts
+++ src/TRAISI/ClientApp/src/survey-viewer-app/app/modules/authorization/saml2/saml2-authorization.component.ts
@@ -1,11 +1,8 @@
-import { Component } from "@angular/core";
-
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'traisi-saml2-authorization-component',
-    template: require('saml2-authorization.component.html'),
-    styles: require('saml2-authorization.component.scss')
+	selector: 'traisi-saml2-authorization-component',
+	template: require('saml2-authorization.component.html'),
+	styles: require('saml2-authorization.component.scss')
 })
 export class Saml2AuthorizationComponent {
-
-}
