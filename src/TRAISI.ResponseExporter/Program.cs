a118be700a6325fa50cca297d7e17d751f6c3572
Brendan Reilly <brendan.benting@gmail.com>
2019-10-09 16:08:51 -0400

    Added CommandLine

--- src/TRAISI.ResponseExporter/Program.cs
+++ src/TRAISI.ResponseExporter/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-
-namespace Traisi.ResponseExporter
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
-        }
-    }
+using CommandLine;
+namespace Traisi.ResponseExporter {
+	public class Options {
+		[Option ('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+		public bool Verbose { get; set; }
+	}
+	class Program {
+		static void Main (string[] args) {
+			Parser.Default.ParseArguments<Options> (args)
+				.WithParsed<Options> (o => {
+					if (o.Verbose) {
+						Console.WriteLine ($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+						Console.WriteLine ("TRAISI - Response Exporter -  App is in Verbose mode!");
+					} else {
+						Console.WriteLine ($"Current Arguments: -v {o.Verbose}");
+						Console.WriteLine ("TRAISI - Response Exporter");
+					}
+				});
+		}
+	}
 }
