7d4f15c9ee5350fa27ae886705d9ad466cea56d4
Brendan Reilly <brendan.reilly@utoronto.ca>
2020-08-11 12:07:43 -0400

    MIgrations added for additional survey user information

---
+++ src/Traisi/Migrations/20200811160617_AddSurveyUserHomeInformation.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+namespace Traisi.Migrations
+{
+    public partial class AddSurveyUserHomeInformation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HomeAddress",
+                table: "AspNetUsers",
+                type: "jsonb",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Point>(
+                name: "HomeLocation",
+                table: "AspNetUsers",
+                type: "geography",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HomeAddress",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "HomeLocation",
+                table: "AspNetUsers");
+        }
+    }
+}
