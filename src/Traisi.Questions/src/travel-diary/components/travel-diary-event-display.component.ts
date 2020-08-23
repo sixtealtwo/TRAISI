5ed940fb97ded5c16cbf5f304fe5728f67228700
Brendan Reilly <brendan.reilly@utoronto.ca>
2020-08-22 22:13:49 -0400

    Updated rendering for special events

--- src/Traisi.Questions/src/travel-diary/components/travel-diary-event-display.component.ts
+++ src/Traisi.Questions/src/travel-diary/components/travel-diary-event-display.component.ts
@@ -1,19 +1,24 @@
-import { Component, ViewEncapsulation, OnChanges, SimpleChanges, OnInit, Input } from '@angular/core';
+import { Component, ViewEncapsulation, OnChanges, SimpleChanges, OnInit, Input, HostBinding } from '@angular/core';
 import { CalendarWeekViewComponent, CalendarEvent } from 'angular-calendar';
 import templateString from './travel-diary-event-display.component.html';
 import styleString from './travel-diary-event-display.component.scss';
+import { WeekViewTimeEvent } from 'calendar-utils';
 @Component({
 	// tslint:disable-line max-classes-per-file
 	selector: 'traisi-travel-diary-event-display',
 	template: '' + templateString,
-	encapsulation: ViewEncapsulation.None,
 	styles: ['' + styleString],
 	providers: [],
 })
 export class TravelDiaryEventDisplayComponent implements OnInit {
-	@Input() public timeEvent: CalendarEvent;
+    @Input() public timeEvent: WeekViewTimeEvent;
+    
+    @HostBinding('class') public class = '';
 
 	public ngOnInit(): void {
+        if(this.timeEvent.event.meta.homeAllDay) {
+            this.class = 'event-home-all-day'
+        }
         console.log(this.timeEvent);
     }
 }
