9eb1965d023b516db4f8a74a161547dfe09f4d0f
Brendan Reilly <brendan.reilly@utoronto.ca>
2020-09-18 18:00:57 -0400

    Added module / es5 environment file for survey-viewer-app

--- src/Traisi.ClientApp/src/survey-viewer-app/main.ts
+++ src/Traisi.ClientApp/src/survey-viewer-app/main.ts
@@ -1,8 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import '@angular/compiler'; 
+import '@angular/compiler';
 import { AppModule } from 'app/app.module';
 import { environment } from 'environments/environment';
+import { module } from 'environments/module.es5';
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +11,7 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+
+  console.log(environment);
+  console.log(module);
