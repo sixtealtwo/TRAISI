5b78ef2ae0a2f034b6ba90b516f2de67e675b70e
Brendan Reilly <brendan.benting@gmail.com>
2020-09-24 09:12:47 -0400

    Next should always be enabled

--- src/Traisi.ClientApp/src/survey-viewer-app/app/modules/survey-navigation/services/survey-navigator/survey-navigator.service.ts
+++ src/Traisi.ClientApp/src/survey-viewer-app/app/modules/survey-navigation/services/survey-navigator/survey-navigator.service.ts
@@ -115,7 +115,7 @@ export class SurveyNavigator {
 		return new Observable((obs) => {
 			this.navigateToPage(this._state.viewerState.surveyPages[0].id).subscribe((v) => {
 				obs.complete();
-				this.nextEnabled$.next(false);
+				// this.nextEnabled$.next(false);
 			});
 		});
 	}
