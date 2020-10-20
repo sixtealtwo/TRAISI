2631906f3d99f0e6fe2f39a6e25eb0e0af0e6971
Brendan Reilly <brendan.reilly@utoronto.ca>
2020-10-20 13:13:07 -0400

    Updated location debounce time

--- src/Traisi.Questions/src/shared/components/location-lookup.component.ts
+++ src/Traisi.Questions/src/shared/components/location-lookup.component.ts
@@ -39,7 +39,7 @@ export class LocationLookupComponent implements OnInit {
 			this.addressInput$.pipe(
 				distinctUntilChanged(),
 				tap(() => (this.addressesLoading = true)),
-				debounceTime(1500),
+				debounceTime(1200),
 				switchMap((term) =>
 					this._geoService.addressCompletion(term).pipe(
 						catchError((err) => {
